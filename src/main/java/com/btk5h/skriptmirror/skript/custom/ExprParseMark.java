4bfa6db6e4fa09f5e10e8c17aec356609588c906
Bryan Terce <bryan.terce@gmail.com>
2017-12-29 08:22:57 -0800

    Add missing parser mark expression

---
+++ src/main/java/com/btk5h/skriptmirror/skript/custom/ExprParseMark.java
@@ -0,0 +1,54 @@
+package com.btk5h.skriptmirror.skript.custom;
+
+import org.bukkit.event.Event;
+
+import ch.njol.skript.ScriptLoader;
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.log.ErrorQuality;
+import ch.njol.util.Kleenean;
+
+public class ExprParseMark extends SimpleExpression<Number> {
+  static {
+    Skript.registerExpression(ExprParseMark.class, Number.class, ExpressionType.SIMPLE,
+        "[the] [parse[r]] mark");
+  }
+
+  @Override
+  protected Number[] get(Event e) {
+    return new Number[]{((CustomSyntaxEvent) e).getParseResult().mark};
+  }
+
+  @Override
+  public boolean isSingle() {
+    return true;
+  }
+
+  @Override
+  public Class<? extends Number> getReturnType() {
+    return Number.class;
+  }
+
+  @Override
+  public String toString(Event e, boolean debug) {
+    return "parser mark";
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed,
+                      SkriptParser.ParseResult parseResult) {
+    if (!ScriptLoader.isCurrentEvent(
+        CustomEffect.EffectEvent.class,
+        CustomExpression.ExpressionGetEvent.class,
+        CustomExpression.ExpressionChangeEvent.class
+    )) {
+      Skript.error("The parser mark may only be used in custom syntax.",
+          ErrorQuality.SEMANTIC_ERROR);
+    }
+    return true;
+  }
+}
