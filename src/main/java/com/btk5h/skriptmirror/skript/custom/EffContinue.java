db62f5a35429039f91c50c0c803198a510c1fb0e
Bryan Terce <bryan.terce@gmail.com>
2018-04-16 18:46:43 -0700

    Add conditional continue to conditions and effects (fixes #45)

--- src/main/java/com/btk5h/skriptmirror/skript/custom/EffContinue.java
+++ src/main/java/com/btk5h/skriptmirror/skript/custom/EffContinue.java
@@ -2,18 +2,18 @@ package com.btk5h.skriptmirror.skript.custom;
 
 import ch.njol.skript.ScriptLoader;
 import ch.njol.skript.Skript;
-import ch.njol.skript.lang.Effect;
-import ch.njol.skript.lang.Expression;
-import ch.njol.skript.lang.SkriptParser;
-import ch.njol.skript.lang.TriggerItem;
+import ch.njol.skript.lang.*;
 import ch.njol.skript.log.ErrorQuality;
 import ch.njol.util.Kleenean;
 import org.bukkit.event.Event;
 
 public class EffContinue extends Effect {
   static {
-    Skript.registerEffect(EffContinue.class, "continue");
+    Skript.registerEffect(EffContinue.class, "continue [(if|when) (%-boolean%|<.+>)]");
   }
+
+  private Expression<Boolean> condition;
+  private Condition skriptCondition;
 
   @Override
   protected void execute(Event e) {
@@ -22,6 +22,11 @@ public class EffContinue extends Effect {
 
   @Override
   protected TriggerItem walk(Event e) {
+    if (skriptCondition != null && !skriptCondition.check(e)
+        || condition != null && condition.getSingle(e) != Boolean.TRUE) {
+      return null;
+    }
+
     if (e instanceof CustomEffect.EffectEvent) {
       if (((CustomEffect.EffectEvent) e).isSync()) {
         Skript.warning("Synchronous events should not be continued. " +
@@ -33,6 +38,7 @@ public class EffContinue extends Effect {
     } else if (e instanceof CustomCondition.ConditionEvent) {
       ((CustomCondition.ConditionEvent) e).markContinue();
     }
+
     return null;
   }
 
@@ -48,6 +54,15 @@ public class EffContinue extends Effect {
     if (!ScriptLoader.isCurrentEvent(CustomEffect.EffectEvent.class, CustomCondition.ConditionEvent.class)) {
       Skript.error("Return may only be used in custom effects and conditions.", ErrorQuality.SEMANTIC_ERROR);
       return false;
+    }
+
+    condition = (Expression<Boolean>) exprs[0];
+
+    if (parseResult.regexes.size() > 0) {
+      String group = parseResult.regexes.get(0).group();
+      skriptCondition = Condition.parse(group, "Can't understand this condition: " + group);
+
+      return skriptCondition != null;
     }
 
     return true;
