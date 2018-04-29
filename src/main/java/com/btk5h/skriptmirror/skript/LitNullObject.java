02c314161211fac7def7d2b0a035cabce14e5723
Bryan Terce <bryan.terce@gmail.com>
2018-04-29 10:24:12 -0700

    Remove unnecessary method overrides

--- src/main/java/com/btk5h/skriptmirror/skript/LitNullObject.java
+++ src/main/java/com/btk5h/skriptmirror/skript/LitNullObject.java
@@ -1,15 +1,9 @@
 package com.btk5h.skriptmirror.skript;
 
-import com.btk5h.skriptmirror.Null;
-
-import org.bukkit.event.Event;
-
 import ch.njol.skript.Skript;
-import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
-import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleLiteral;
-import ch.njol.util.Kleenean;
+import com.btk5h.skriptmirror.Null;
 
 public class LitNullObject extends SimpleLiteral<Null> {
   static {
@@ -18,16 +12,5 @@ public class LitNullObject extends SimpleLiteral<Null> {
 
   public LitNullObject() {
     super(Null.getInstance(), false);
-  }
-
-  @Override
-  public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed,
-                      SkriptParser.ParseResult parseResult) {
-    return true;
-  }
-
-  @Override
-  public String toString(Event e, boolean debug) {
-    return "null";
   }
 }
