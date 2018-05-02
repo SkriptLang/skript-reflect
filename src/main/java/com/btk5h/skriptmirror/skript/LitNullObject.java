4b44232acf20ef9ed4290b2ee502079c5c7a9447
Bryan Terce <bryan.terce@gmail.com>
2018-05-02 10:42:02 -0700

    Revert "Remove unnecessary method overrides"
    This reverts commit 02c3141
    
    Whoops, it was necessary!

--- src/main/java/com/btk5h/skriptmirror/skript/LitNullObject.java
+++ src/main/java/com/btk5h/skriptmirror/skript/LitNullObject.java
@@ -1,9 +1,13 @@
 package com.btk5h.skriptmirror.skript;
 
 import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleLiteral;
+import ch.njol.util.Kleenean;
 import com.btk5h.skriptmirror.Null;
+import org.bukkit.event.Event;
 
 public class LitNullObject extends SimpleLiteral<Null> {
   static {
@@ -12,5 +16,16 @@ public class LitNullObject extends SimpleLiteral<Null> {
 
   public LitNullObject() {
     super(Null.getInstance(), false);
+  }
+
+  @Override
+  public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed,
+                      SkriptParser.ParseResult parseResult) {
+    return true;
+  }
+
+  @Override
+  public String toString(Event e, boolean debug) {
+    return "null";
   }
 }
