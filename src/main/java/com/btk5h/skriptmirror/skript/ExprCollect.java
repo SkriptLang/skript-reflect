6fab07403ffa4727484548d316e20cb4662ca13e
Bryan Terce <bryan.terce@gmail.com>
2018-06-06 10:58:12 -0700

    Integrate casting into the collect expression (fixes #67)

--- src/main/java/com/btk5h/skriptmirror/skript/ExprCollect.java
+++ src/main/java/com/btk5h/skriptmirror/skript/ExprCollect.java
@@ -6,6 +6,7 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.util.Kleenean;
+import com.btk5h.skriptmirror.JavaType;
 import com.btk5h.skriptmirror.Null;
 import com.btk5h.skriptmirror.ObjectWrapper;
 import com.btk5h.skriptmirror.util.JavaUtil;
@@ -15,21 +16,31 @@ import org.bukkit.event.Event;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 public class ExprCollect extends SimpleExpression<ObjectWrapper> {
   static {
-    Skript.registerExpression(ExprCollect.class, ObjectWrapper.class, ExpressionType.COMBINED, "\\[%objects%\\]");
+    Skript.registerExpression(ExprCollect.class, ObjectWrapper.class, ExpressionType.COMBINED,
+        "\\[%objects%[ as %-javatype%[ ]]\\]");
   }
 
   private Expression<Object> objects;
+  private Expression<JavaType> type;
 
   @Override
   protected ObjectWrapper[] get(Event e) {
-    Object[] items =
-        Arrays.stream(objects.getArray(e))
-            .map(o -> o instanceof Null ? null : o)
-            .map(o -> o instanceof ObjectWrapper ? ((ObjectWrapper) o).get() : o)
-            .toArray();
+    Stream<Object> objectStream = Arrays.stream(objects.getArray(e))
+        .map(o -> o instanceof Null ? null : o)
+        .map(o -> o instanceof ObjectWrapper ? ((ObjectWrapper) o).get() : o);
+
+    if (type != null) {
+      JavaType componentType = type.getSingle(e);
+      if (componentType != null) {
+        objectStream = objectStream.filter(o -> o == null || componentType.getJavaClass().isInstance(o));
+      }
+    }
+
+    Object[] items = objectStream.toArray();
     Object[] castedItems = JavaUtil.newArray(getCommonSuperclass(items), items.length);
 
     System.arraycopy(items, 0, castedItems, 0, items.length);
@@ -80,6 +91,7 @@ public class ExprCollect extends SimpleExpression<ObjectWrapper> {
   public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed,
                       SkriptParser.ParseResult parseResult) {
     objects = SkriptUtil.defendExpression(exprs[0]);
+    type = (Expression<JavaType>) exprs[1];
     return SkriptUtil.canInitSafely(objects);
   }
 }
