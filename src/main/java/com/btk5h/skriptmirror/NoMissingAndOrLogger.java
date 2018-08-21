bc94ad99294ec301eba6805afb0733dcbcee347e
Bryan Terce <bryan.terce@gmail.com>
2018-08-21 13:18:48 -0700

    Fix compatibility with other addons that have custom sections

--- src/main/java/com/btk5h/skriptmirror/NoMissingAndOrLogger.java
+++ src/main/java/com/btk5h/skriptmirror/NoMissingAndOrLogger.java
@@ -1,10 +1,10 @@
 package com.btk5h.skriptmirror;
 
 import ch.njol.skript.log.LogEntry;
-import ch.njol.skript.log.LogHandler;
+import ch.njol.skript.log.ParseLogHandler;
 import com.btk5h.skriptmirror.util.SkriptReflection;
 
-public class NoMissingAndOrLogger extends LogHandler {
+public class NoMissingAndOrLogger extends ParseLogHandler {
   @Override
   public LogResult log(LogEntry entry) {
     if (SkriptReflection.MISSING_AND_OR != null && entry.message.contains(SkriptReflection.MISSING_AND_OR)) {
