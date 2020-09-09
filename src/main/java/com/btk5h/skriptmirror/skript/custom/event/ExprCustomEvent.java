112dd485040aa00c9a98ebfbcc0098115082fc73
HYPExMon5ter <BrackettBlake@gmail.com>
2020-09-09 16:03:15 -0400

    Update ExprCustomEvent.java

--- src/main/java/com/btk5h/skriptmirror/skript/custom/event/ExprCustomEvent.java
+++ src/main/java/com/btk5h/skriptmirror/skript/custom/event/ExprCustomEvent.java
@@ -8,6 +8,8 @@ import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.Variable;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.registrations.Classes;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Name;
 import ch.njol.util.Kleenean;
 import com.btk5h.skriptmirror.util.SkriptUtil;
 import org.bukkit.event.Event;
