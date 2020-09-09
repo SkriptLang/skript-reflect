18251ae3973877cafe9eeec6f4cd9f2b44230579
HYPExMon5ter <BrackettBlake@gmail.com>
2020-09-09 15:57:42 -0400

    Update ExprMatchedPattern.java

--- src/main/java/com/btk5h/skriptmirror/skript/custom/ExprMatchedPattern.java
+++ src/main/java/com/btk5h/skriptmirror/skript/custom/ExprMatchedPattern.java
@@ -7,6 +7,8 @@ import ch.njol.skript.lang.ExpressionType;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.skript.lang.util.SimpleExpression;
 import ch.njol.skript.log.ErrorQuality;
+import ch.njol.skript.doc.Description;
+import ch.njol.skript.doc.Name;
 import ch.njol.util.Kleenean;
 import com.btk5h.skriptmirror.skript.custom.condition.ConditionCheckEvent;
 import com.btk5h.skriptmirror.skript.custom.effect.EffectTriggerEvent;
