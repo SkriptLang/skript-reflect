1eb38b8be3a6a9b29846521e04bd18976f355438
Jake <gabrieleluparelloj@gmail.com>
2025-01-16 03:46:45 +0100

    Added Horizontal Ellipsis character to the spread expression.
    Adds the … character to ExprSpread, this character is usually used in IOS devices, this pr allows people using those devices to use this character.

--- src/main/java/com/btk5h/skriptmirror/skript/ExprSpread.java
+++ src/main/java/com/btk5h/skriptmirror/skript/ExprSpread.java
@@ -25,7 +25,7 @@ import java.util.stream.Stream;
 public class ExprSpread<T> implements Expression<T> {
   static {
     //noinspection unchecked
-    Skript.registerExpression(ExprSpread.class, Object.class, ExpressionType.COMBINED, "...%object%");
+    Skript.registerExpression(ExprSpread.class, Object.class, ExpressionType.COMBINED, "...%object%", "…%object%");
   }
 
   private Expression<Object> object;
